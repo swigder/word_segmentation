b3ba96b8db91ce670ccd81ac47a5be7b234836b2
suri <swigder@gmail.com>
2015-10-24 20:34:11 -0400

    updating docs

--- word_segmentation/bigram_word_segmenter.py
+++ word_segmentation/bigram_word_segmenter.py
@@ -3,17 +3,17 @@ from utilities.utilities import bisect_string
 
 class BigramWordSegmenter:
     """
-    Segments a sentence containing no spaces into a list of words, by globally maximizing the frequency of words in the
-    segmentation as well as favoring longer words.  Each segmentation is given a score, which is the sum of the logs of
-    the frequencies of the words divided by the number of words.  Using the log of the frequencies of the words ensures
-    that very frequent words, such as "the", don't overwhelm the entire scoring.  In order to favor longer words, and so
-    that the addition of logs (a trick to avoid recomputing scores for every combination) does not overwhelmingly favor
-    many shorter words over fewer longer words.
+    Segments a sentence containing no spaces into a list of words, by finding the segmentation that maximizes the bigram
+    probability.  The score for of each possible sentence is the bigram probability of the sentence (probability of the
+    first word times product of the probabilities of all bigrams), multiplied by the number of words in the sentence.
+    The last multiplication helped compensate for fact that a sentence with many words will have many more fraction
+    multiplication.
     """
 
     def __init__(self, unigram_provider, bigram_provider):
         """
-        :param unigram_provider: unigram provider that can provide the most frequent word of a word list
+        :param unigram_provider: unigram provider that can provide frequency counts for words in a corpus
+        :param bigram_provider: bigram provider that can provide frequency counts for bigrams in a corpus
         """
         self.unigram_provider = unigram_provider
         self.bigram_provider = bigram_provider
@@ -21,8 +21,8 @@ class BigramWordSegmenter:
 
     def segment_words(self, string):
         """
-        Segments a sentence into words using by optimizing unigram counts globally as well as favoring long words over
-        short ones.
+        Segments a sentence into words by optimizing the bigram probability of the sentence.  Uses dynamic programming
+        under the hood.
         :param string: words without spaces separating them
         :return: list of words that are a word segmentation of the given string
         """
@@ -31,9 +31,8 @@ class BigramWordSegmenter:
 
     def segment_words_dynamically(self, string, cache):
         """
-        Segments a sentence into words using by optimizing unigram counts globally as well as favoring long words over
-        short ones. Uses dynamic programming under the hood, adding to the cache of best segmentation for the given
-        string and all substrings.
+        Segments a sentence into words by optimizing the bigram probability of the sentence. Uses dynamic programming
+        under the hood, adding to the cache of best segmentation for the given string and all substrings.
         This method is called recursively.
         :param string: words without spaces separating them
         :param cache: cache of best segmentations for the string (or any other strings in the global string)
@@ -57,8 +56,8 @@ class BigramWordSegmenter:
             new_bigram = tuple([segmentation_a[-1], segmentation_b[0]])
             frequency_first_word = max(self.unigram_provider.get_frequency(segmentation_a[-1]), 1)
             probability_new_bigram = max(1, self.bigram_provider.get_frequency(new_bigram)) / frequency_first_word
-            score = score_a * score_b * probability_new_bigram * len(segmentation_a + segmentation_b)
-            if score > best_score:
+            score = score_a * score_b * probability_new_bigram
+            if score * len(segmentation_a + segmentation_b) > best_score:
                 best_score = score
                 best_segmentation = segmentation_a + segmentation_b
 
